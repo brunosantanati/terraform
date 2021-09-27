edf64e9c092e5c8a61a51d251af18a3fb4cf4c58
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-09-27 17:37:08 -0300

    Criando recursos em outra region (us-east-2).

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "dev" {
   count = 3
   ami = "ami-0747bdcabd34c712a"
@@ -33,6 +39,17 @@ resource "aws_instance" "dev5" {
     Name = "dev5"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev6" {
+  provider = "aws.us-east-2"
+  ami = "ami-00dfe2c7ce89a450b"
+  instance_type = "t2.micro"
+  key_name = "terraform-aws"
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
 }
 
 resource "aws_s3_bucket" "dev4" {
--- security-group.tf
+++ security-group.tf
@@ -13,3 +13,20 @@ resource "aws_security_group" "acesso-ssh" {
     Name = "ssh"
   }
 }
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = "aws.us-east-2"
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["201.26.192.50/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
