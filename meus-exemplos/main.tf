bc4da3d944b9e25d47044d086d0be3ed9ef86514
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-09-29 13:47:23 -0300

    Separar em dois arquivos para melhor organizacao.

--- meus-exemplos/main.tf
+++ meus-exemplos/main.tf
@@ -1,28 +1,3 @@
 provider "aws" {
   region  = "us-east-1"
 }
-
-resource "aws_s3_bucket" "openbanking-bucket" {
-  bucket = "openbanking-bucket"
-  acl    = "private"
-
-  tags = {
-    Name = "openbanking-bucket"
-  }
-}
-
-resource "aws_s3_bucket_object" "openbanking-folders" {
-  count  = "${length(var.s3_folders)}"
-  bucket = "openbanking-bucket"
-  acl    = "private"
-  key    = "${var.s3_folders[count.index]}/"
-  #source = "/dev/null" # Linux
-  source = "nul" # Windows
-  depends_on = [
-    aws_s3_bucket.openbanking-bucket
-  ]
-
-  tags = {
-    Name = "openbanking-folders"
-  }
-}
---
+++ meus-exemplos/s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "openbanking-bucket" {
+  bucket = "openbanking-bucket"
+  acl    = "private"
+
+  tags = {
+    Name = "openbanking-bucket"
+  }
+}
+
+resource "aws_s3_bucket_object" "openbanking-folders" {
+  count  = "${length(var.s3_folders)}"
+  bucket = "openbanking-bucket"
+  acl    = "private"
+  key    = "${var.s3_folders[count.index]}/"
+  #source = "/dev/null" # Linux
+  source = "nul" # Windows
+  depends_on = [
+    aws_s3_bucket.openbanking-bucket
+  ]
+
+  tags = {
+    Name = "openbanking-folders"
+  }
+}
